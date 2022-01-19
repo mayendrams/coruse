be6c862bf12a57c0a999fc51ea94415c8df3f19a
mayendrams <mayendrams@gmail.com>
2022-01-19 09:23:49 +0700

    create UserDto in shared layer,UserRest as response model, UserDetailsRequest as request model

--- mobile-app-ws/src/main/java/com/ecquaria/app/ws/ui/controller/UserController.java
+++ mobile-app-ws/src/main/java/com/ecquaria/app/ws/ui/controller/UserController.java
@@ -4,8 +4,12 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ecquaria.app.ws.ui.model.request.UserDetailsRequestModel;
+import com.ecquaria.app.ws.ui.model.response.UserRest;
 
 @RestController
 @RequestMapping("users") // http://localhost:8080/users
@@ -15,20 +19,20 @@ public class UserController {
 	public String getUser() {
 		return "get user was called";
 	}
-	
+
 	@PostMapping
-	public String createUser() {
-		return "create user was called";
+	public UserRest createUser(@RequestBody UserDetailsRequestModel userDetails) {
+		return null;
 	}
-	
+
 	@PutMapping
 	public String updateUser() {
 		return "update user was called";
 	}
-	
+
 	@DeleteMapping
 	public String deleteUser() {
 		return "delete user was called";
 	}
-	
+
 }
