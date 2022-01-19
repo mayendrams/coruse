500b607d677c74d68a3d77ee811b9ecf486d0b26
mayendrams <mayendrams@gmail.com>
2022-01-19 10:23:13 +0700

    create UserRepo, UserServiceImpl to setEncryptpass and userId

---
+++ mobile-app-ws/src/main/java/com/ecquaria/app/ws/UserRepository.java
@@ -0,0 +1,11 @@
+package com.ecquaria.app.ws;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ecquaria.app.ws.io.entity.UserEntity;
+
+@Repository
+public interface UserRepository extends CrudRepository<UserEntity, Long> {
+
+}
