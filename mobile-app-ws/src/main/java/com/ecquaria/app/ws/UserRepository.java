391e96aa8e27733578ac097bce38fbfd7d07fb4f
mayendrams <mayendrams@gmail.com>
2022-01-19 11:48:40 +0700

    create Utils to generate random userId

--- mobile-app-ws/src/main/java/com/ecquaria/app/ws/UserRepository.java
+++ mobile-app-ws/src/main/java/com/ecquaria/app/ws/UserRepository.java
@@ -8,4 +8,6 @@ import com.ecquaria.app.ws.io.entity.UserEntity;
 @Repository
 public interface UserRepository extends CrudRepository<UserEntity, Long> {
 
+	UserEntity findByEmail(String email);
+	
 }
--- mobile-app-ws/src/main/java/com/ecquaria/app/ws/service/impl/UserServiceImpl.java
+++ mobile-app-ws/src/main/java/com/ecquaria/app/ws/service/impl/UserServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import com.ecquaria.app.ws.UserRepository;
 import com.ecquaria.app.ws.io.entity.UserEntity;
 import com.ecquaria.app.ws.service.UserService;
+import com.ecquaria.app.ws.shared.Utils;
 import com.ecquaria.app.ws.shared.dto.UserDto;
 
 @Service
@@ -15,14 +16,22 @@ public class UserServiceImpl implements UserService {
 	@Autowired
 	UserRepository userRepository;
 	
+	@Autowired
+	Utils utils;
+	
 	@Override
 	public UserDto createUser(UserDto user) {
 
+		
+		if (userRepository.findByEmail(user.getEmail()) != null) throw new RuntimeException("Record already exists");
+		
 		UserEntity userEntity = new UserEntity();
 		BeanUtils.copyProperties(user, userEntity);
 		
+		
+		String publicUserId = utils.generatedUserId(30);
+		userEntity.setUserId(publicUserId);
 		userEntity.setEncryptedPassword("test");
-		userEntity.setUserId("testUserId");
 		
 		UserEntity storedUserDetails = userRepository.save(userEntity);
 		
